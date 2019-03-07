16dcb8eb4eacafa2f913549673d80e22b08b11c2
Andrii <andrii.sushket@logivations.com>
2019-03-07 17:49:29 +0200

    left panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {Button, ListGroup, ListGroupItem, Collapse} from 'react-bootstrap';
+import SlidingPane from 'react-sliding-pane';
+import {BrowserRouter, Route, Link} from "react-router-dom";
 import './App.css';
+import 'react-sliding-pane/dist/react-sliding-pane.css';
 
 import {SomeWorks} from "./works/Works.jsx";
 import {Codevember} from "./codevember/Codevember.jsx";
@@ -8,9 +11,52 @@ import {Shaders} from "./shaders/Shaders.jsx";
 
 class App extends React.Component {
 
-    render() {
-    	return (
-    		<div className="App" style={{overflow: "hidden"}}>
+	constructor(){
+		super();
+		this.state = {
+			isPaneOpen: false,
+			open: false
+		}
+	}
+
+	render() {
+		const {open, isPaneOpen} = this.state;
+		return (
+			<div className="App" style={{overflow: "hidden"}}>
+				<div style={{ position: "absolute", marginTop: '32px' }}>
+					<Button onClick={ () => this.setState({ isPaneOpen: true }) }>
+						Select
+					</Button>
+				</div>
+				<SlidingPane from='left'
+								 closeIcon={<div>Icon</div>}
+								 isOpen={isPaneOpen}
+								 width='200px'
+								 onRequestClose={() => this.setState({isPaneOpen: false})}>
+					<Button
+						onClick={() => this.setState({ open: !open })}
+						aria-controls="example-collapse-text"
+						aria-expanded={open}
+					><div>CodeVember</div>
+
+							<Collapse in={open}>
+								<div>
+								{/*<ListGroup>*/}
+								<a href={"/Shader/Shader4"}>Shader4</a>
+								{/*<ListGroupItem><a href={"/Shader/Shader4"}>Shader4</a></ListGroupItem>*/}
+								{/*<ListGroupItem><a href={"/Codevember/Galaxy"}>Galaxy</a></ListGroupItem>*/}
+								{/*<ListGroupItem>"Morbi leo risus"</ListGroupItem>*/}
+								{/*<ListGroupItem>"Porta ac consectetur ac"</ListGroupItem>*/}
+								{/*<ListGroupItem>"Vestibulum at eros"</ListGroupItem>*/}
+								{/*</ListGroup>*/}
+								</div>
+							</Collapse>
+
+					</Button>
+
+
+
+				</SlidingPane>
 				<BrowserRouter>
 					<div>
 						<Route path="/Codevember" component={Codevember} />
@@ -21,7 +67,7 @@ class App extends React.Component {
 					</div>
 				</BrowserRouter>
 			</div>);
-    }
+	}
 }
 
 export default App;
